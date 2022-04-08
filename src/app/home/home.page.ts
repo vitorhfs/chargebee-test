c41aa33cf65ac28d2d3bdb22e8b02ac010214f59
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-04-08 18:19:55 -0300

    feat(app): clean up initial project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +6,5 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
-
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
-  }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
-
+  constructor() {}
 }
